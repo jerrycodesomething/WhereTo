98337573ae9ce4a77bab5db7f23a5443a7dca826
Yuwei <guoyuwei200513@gmail.com>
2021-01-10 00:11:07 +0800

    I printed "hiiii"

--- app/src/main/java/com/example/whereto/MainActivity.java
+++ app/src/main/java/com/example/whereto/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("HAHA I DELETED EVERYTHING");
         System.out.println("gogol");
+        System.out.println("hihihi");
     }
 }
