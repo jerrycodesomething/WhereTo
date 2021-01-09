7a8443df4809a25a5922511e197ac04359170068
jerry <jerryasian1@gmail.com>
2021-01-10 00:23:27 +0800

    Jerry wants to commit this

--- app/src/main/java/com/example/whereto/MainActivity.java
+++ app/src/main/java/com/example/whereto/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("HAHA I DELETED EVERYTHING");
-        System.out.println("THIS IS HERWKLSDJFLKSJD FLKASDF ASDF AS");
-        System.out.println("heheheh");
+        System.out.println("goodbye");
     }
 }
