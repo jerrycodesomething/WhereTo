5509ebd127138ab3014f58fcc5855b0d334e8731
jerry <jerryasian1@gmail.com>
2021-01-21 13:08:16 +0800

    1) Fixed fullscreen map view

--- app/src/main/java/com/example/whereto/homepage.java
+++ app/src/main/java/com/example/whereto/homepage.java
@@ -4,25 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    LinearLayout contentView;
-    static final float END_SCALE = 0.7f;
-    ImageView menuIcon,backtoLoginPage,iv_checkin;
-
-
-    //Drawer menu
+    //Homepage components
+    FloatingActionButton menuIcon;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
 
@@ -36,36 +29,18 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
         fragment = new FragmentMap();
         getSupportFragmentManager().beginTransaction().replace(R.id.map_output, fragment).commit();
 
-        iv_checkin = findViewById(R.id.iv_checkin);
-        backtoLoginPage = findViewById(R.id.backtoLogin);
-        contentView = findViewById(R.id.content);
+        //button finders
         menuIcon = findViewById(R.id.sidebarMenu);
 
         //Menu Hooks
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
 
-        backtoLoginPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(homepage.this,LogIn.class));
-            }
-        });
-
-        iv_checkin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(homepage.this,CheckinActivity.class);
-                startActivity(intent);
-            }
-        });
-
         navigationDrawer();
-
     }
 
 
-    //Navigation Drawer Function
+    //Navigation Drawer Method
     private void navigationDrawer() {
         //Navigation drawer
         navigationView.bringToFront();  //want to interact with the navigation drawer
@@ -80,29 +55,6 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
                 else {
                     drawerLayout.openDrawer(GravityCompat.START);
                 }
-            }
-        });
-
-        animationForNavigationDrawer();
-    }
-
-
-    private void animationForNavigationDrawer() {
-        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
-            @Override
-            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-
-                //scale the view based on current slide offset
-                final float diffScaledOffset = slideOffset*(1 - END_SCALE);
-                final float offsetScale = 1 - diffScaledOffset;
-                contentView.setScaleX(offsetScale);
-                contentView.setScaleY(offsetScale);
-
-                //translate the view, accounting for the scaled width
-                final float xOffset = drawerView.getWidth()*slideOffset;
-                final float xOffsetDiff = contentView.getWidth()*diffScaledOffset/2;
-                final float xTranslation = xOffset - xOffsetDiff;
-                contentView.setTranslationX(xTranslation);
             }
         });
     }
