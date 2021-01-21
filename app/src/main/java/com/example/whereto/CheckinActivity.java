e785a792804dfc40c24421613171d4d61d1ef669
huang yifei <ailisi524@126.com>
2021-01-21 17:06:24 +0800

    Huang Yifei:privacy /friend/public

--- app/src/main/java/com/example/whereto/CheckinActivity.java
+++ app/src/main/java/com/example/whereto/CheckinActivity.java
@@ -5,8 +5,11 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,9 @@ public class CheckinActivity extends AppCompatActivity {
     private Button btn_camera;
     private ImageView iv_camera;
     private final int CAMERA_REQUEST = 10;
+    private Button btn_privacy;
+    private PopupWindow pw_privacy;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +47,37 @@ public class CheckinActivity extends AppCompatActivity {
                         }
                         break;
                 }
+            }
+        });
+
+        btn_privacy = findViewById(R.id.bt_share);
+        btn_privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view =  getLayoutInflater().inflate(R.layout.activity_sharing_privacy_setting,null);
+                pw_privacy = new PopupWindow(view,btn_privacy.getWidth(), ViewGroup.LayoutParams.WRAP_CONTENT);
+                pw_privacy.setOutsideTouchable(true);
+                pw_privacy.showAsDropDown(btn_privacy);
+
+                //set public onclick
+                TextView textView = view.findViewById(R.id.tv_public);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pw_privacy.dismiss();
+                        btn_privacy.setText(textView.getText());
+                    }
+                });
+                //set friends onclick
+                TextView textView_fiend = view.findViewById(R.id.tv_friend);
+                textView_fiend.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pw_privacy.dismiss();
+                        btn_privacy.setText(textView_fiend.getText());
+                    }
+                });
+
             }
         });
     }
