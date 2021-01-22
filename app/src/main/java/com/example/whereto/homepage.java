69af140e3737f9b12536bcff2c2251b2b88daae4
jerry <jerryasian1@gmail.com>
2021-01-22 11:52:56 +0800

    Fixed all fragment nesting issues. All map actions will be handled in the FragmentMap class.

--- app/src/main/java/com/example/whereto/homepage.java
+++ app/src/main/java/com/example/whereto/homepage.java
@@ -30,16 +30,6 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        //Check in
-
-        checkInIcon = findViewById(R.id.check_in_button);
-        checkInIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(homepage.this,CheckinActivity.class);
-                startActivity(intent);
-            }
-        });
 
         // Loads the map fragment
         Fragment fragment = null;
