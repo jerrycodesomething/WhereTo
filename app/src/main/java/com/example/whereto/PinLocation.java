f69e5b5478933cead1df5ad17f6771d8376d44f0
Yuwei <guoyuwei200513@gmail.com>
2021-01-26 11:23:11 +0800

    Launch Map but just open google Map

--- app/src/main/java/com/example/whereto/PinLocation.java
+++ app/src/main/java/com/example/whereto/PinLocation.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PinLocation extends AppCompatActivity {
-    private FloatingActionButton navigation_btn;
+    private Button navigation_btn;
 
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -19,20 +20,12 @@ public class PinLocation extends AppCompatActivity {
         navigation_btn=findViewById(R.id.navigation);
 
         navigation_btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent= new Intent(Intent.ACTION_VIEW,
-                        //jerry see here
-                        Uri.parse("google.navigation: q= "));
-
-
-                intent.setPackage("com.google.android.apps.maps");
-
-
-                if(intent.resolveActivity(getPackageManager())!=null){
-                    startActivity(intent);
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:47.4925,19.0513"));
+                Intent chooser = Intent.createChooser(intent,"launch maps");
+                startActivity(chooser);
             }
-
         });
     }
 }
